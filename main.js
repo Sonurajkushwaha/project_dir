1a25ba40961a173fbf61d70626ead9a96122b279
Sonurajkushwaha <sonurajkushwaha44@gmail.com>
2023-01-22 21:00:44 +0530

    commiting again

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 <script>
 var x, y, z;  // Declare 3 variables
-x = 5;    // Assign the value 5 to x
+x = 7;    // Assign the value 5 to x
 y = 6;    // Assign the value 6 to y
 z = x + y;  // Assign the sum of x and y to z
 
